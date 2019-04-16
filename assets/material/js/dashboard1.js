34b59794588154c95d05c4282cfdb460466947a6
unknown <ralph.degoma@conversant.com.sg>
2019-04-17 06:53:48 +0800

    first commit

---
+++ assets/material/js/dashboard1.js
@@ -0,0 +1,30 @@
+/*
+Template Name: Material Pro Admin
+Author: Themedesigner
+Email: niravjoshi87@gmail.com
+File: js
+*/
+$(function () {
+    "use strict";
+  
+    // ============================================================== 
+    // Download count
+    // ============================================================== 
+    var sparklineLogin = function () {
+        $('.spark-count').sparkline([4, 5, 0, 10, 9, 12, 4, 9, 4, 5, 3, 10, 9, 12, 10, 9, 12, 4, 9], {
+            type: 'bar'
+            , width: '100%'
+            , height: '70'
+            , barWidth: '2'
+            , resize: true
+            , barSpacing: '6'
+            , barColor: 'rgba(255, 255, 255, 0.3)'
+        });
+        
+    }
+    var sparkResize;
+    
+    sparklineLogin();
+    
+    
+});
